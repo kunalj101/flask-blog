e5c2f21f086cf1c7c8a274f7caa2c5b934ed71e2
Kunal Jain <kunalj101@gmail.com>
2016-04-27 10:53:42 +0530

    Added CSS and functionality to add blogs

--- blog.py
+++ blog.py
@@ -44,6 +44,22 @@ def login():
 
 	return render_template('login.html', error = error)
 
+@app.route('/add', methods = ['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	if not title or not post:
+		flash("All fields are required. Please try again.")
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('insert into posts (title, post) values (?, ?)',[request.form['title'], request.form['post']])
+		g.db.commit()
+		g.db.close()
+		flash("New post successfully published")
+		return redirect(url_for('main'))
+
 @app.route('/main')
 @login_required
 def main():
